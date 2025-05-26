1e91296c30c711a39a5073f921ee682997b5832d
kariosslk <abdelkalak73@gmail.com>
2025-05-26 13:57:42 +0200

    Mise en commun Final

--- Assets/WindSound.cs
+++ Assets/WindSound.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -47,4 +47,4 @@ public class WindSound : MonoBehaviour
         if (audioSource.isPlaying)
             audioSource.Stop();
     }
-}
+}*/
